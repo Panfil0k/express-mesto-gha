ae1d43bb11beb0405d8893762f1c2a00ce33f995
Artem Ponamarchuk <panfil0k@yandex.ru>
2022-11-08 18:42:48 +0400

    feat: задачи 13-го спринта

--- routes/cards.js
+++ routes/cards.js
@@ -7,6 +7,6 @@ router.get('/cards', getCards);
 router.post('/cards', createCard);
 router.delete('/cards/:cardId', deleteCard);
 router.put('/cards/:cardId/likes', likeCard);
-router.delete('/cards/:cardId/likes', dislikeCard);
+router.put('/cards/:cardId/likes', dislikeCard);
 
 module.exports = router;
