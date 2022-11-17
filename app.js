0f487b3238de1464e28079b48f83de4799b4ab3e
Artem Ponamarchuk <panfil0k@yandex.ru>
2022-11-17 13:06:53 +0400

    feat: задачи 14-го спринта

--- app.js
+++ app.js
@@ -27,10 +27,10 @@ app.post('/signin', celebrate({
 }), login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().required().uri(),
-    email: Joi.string().required().email(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().uri(),
+    email: Joi.string().email().required(),
     password: Joi.string().required(),
   }).unknown(true),
 }), createUser);
