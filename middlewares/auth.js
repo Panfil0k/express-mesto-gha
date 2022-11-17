9b7a3b1680d8c51a5797498d88ee8b6c71caf734
Artem Ponamarchuk <panfil0k@yandex.ru>
2022-11-17 20:25:18 +0400

    feat: задачи 14-го спринта

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,6 @@
 const jwt = require('jsonwebtoken');
-const crypto = require('crypto');
 const UNAUTHORIZED_ERROR = require('../errors/UnauthorizedError');
 const { MESSAGE_AUTHENTICATION_ERROR } = require('../utils/constants');
-
-const secretKey = crypto
-  .randomBytes(16)
-  .toString('hex');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -14,12 +9,16 @@ module.exports = (req, res, next) => {
     throw new UNAUTHORIZED_ERROR(MESSAGE_AUTHENTICATION_ERROR);
   }
 
-  const token = authorization.replace('Bearer ', '');
+  const token = req.cookies.jwt;
+
+  if (!token) {
+    throw new UNAUTHORIZED_ERROR(MESSAGE_AUTHENTICATION_ERROR);
+  }
 
   let payload;
 
   try {
-    payload = jwt.verify(token, secretKey);
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
     next(new UNAUTHORIZED_ERROR(MESSAGE_AUTHENTICATION_ERROR));
   }
