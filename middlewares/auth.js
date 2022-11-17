32617448b2a0d734be8d5f3cce13476bad73184a
Artem Ponamarchuk <panfil0k@yandex.ru>
2022-11-17 21:05:08 +0400

    feat: задачи 14-го спринта

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ const UNAUTHORIZED_ERROR = require('../errors/UnauthorizedError');
 const { MESSAGE_AUTHENTICATION_ERROR } = require('../utils/constants');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.cookies.jwt;
+  const authorization = req.cookies.jwt;
 
   if (!authorization) {
     throw new UNAUTHORIZED_ERROR(MESSAGE_AUTHENTICATION_ERROR);
