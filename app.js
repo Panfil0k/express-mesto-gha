cce6477c70168af64dd48a0a5a5aca48fb122be0
Artem Ponamarchuk <panfil0k@yandex.ru>
2022-11-23 15:03:25 +0400

    fix: замечания ревью

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-const { celebrate, Joi, errors } = require('celebrate');
+const { errors } = require('celebrate');
 const routes = require('./routes/index');
-const auth = require('./middlewares/auth');
-const {
-  createUser, login,
-} = require('./controllers/users');
 const {
   SERVER_ERROR,
   MESSAGE_SERVER_ERROR,
@@ -22,28 +18,9 @@ app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(DATA_BASE);
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
-
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri({ scheme: ['http', 'https'] }),
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-  }).unknown(true),
-}), createUser);
-
-app.use(auth);
 app.use(routes);
 app.use(errors());
 
-// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   const { statusCode = SERVER_ERROR, message } = err;
 
@@ -52,6 +29,7 @@ app.use((err, req, res, next) => {
       ? MESSAGE_SERVER_ERROR
       : message,
   });
+  next();
 });
 
 app.listen(PORT);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+const auth = require('../middlewares/auth');
+const {
+  createUser, login,
+} = require('../controllers/users');
 const NOT_FOUND_ERROR = require('../errors/NotFoundError');
 
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().uri({ scheme: ['http', 'https'] }),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }).unknown(true),
+}), createUser);
+
+router.use(auth);
 router.use(usersRouter);
 router.use(cardsRouter);
 router.use('*', (req, res, next) => {
